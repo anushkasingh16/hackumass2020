534f829b923abf7871df7b24d5042053e8dd56fd
Anushka Singh <55222063+anushkasingh16@users.noreply.github.com>
2020-12-19 20:14:43 +0530

    react added

---
+++ givetogether/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,500;0,700;1,500&display=swap" rel="stylesheet"></link>
+    <div className="header">
+         <center>giveback</center> 
+         </div>
+        <div className="mainlayout">
+            <div className="categories">
+                <div className="list">
+                    <ul>
+                        <li>Education</li>
+                        <li>Tech</li>
+                        <li>Health</li>
+                        <li>Politics</li>
+                    </ul>
+            </div>
+            </div>
+            <div className="trending"> trending
+            </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
