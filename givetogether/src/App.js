4260c532e870bf5cc4db47dac2ca961d810dca3f
Anushka Singh <55222063+anushkasingh16@users.noreply.github.com>
2020-12-20 00:25:45 +0530

    added firebase, changes in css

--- givetogether/src/App.js
+++ givetogether/src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import firebase from 'firebase';
+import firebaseConfig from './config.js';
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
 
 function App() {
   return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/donate">
+            <Donate />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return (
     <div>
        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,500;0,700;1,500&display=swap" rel="stylesheet"></link>
-    <div className="header">
-         <center>giveback</center> 
-         </div>
+        <div className="header">
+            <center>MinMatch</center> 
+        </div>
         <div className="mainlayout">
             <div className="categories">
                 <div className="list">
@@ -16,13 +41,20 @@ function App() {
                         <li>Health</li>
                         <li>Politics</li>
                     </ul>
+                </div>
             </div>
+            <div className="trending"> < a href="/donate">trending</a>
             </div>
-            <div className="trending"> trending
-            </div>
+            
+    </div>
+    <div className="footer">
+             <h2>About</h2>
     </div>
     </div>
   );
 }
 
+function Donate() {
+  return <h2>Donate</h2>;
+}
 export default App;
---
+++ givetogether/src/config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+var config = {
+  apiKey: 'AIzaSyCTTl5Bf4EwAkuwSikWuhn41_M1rj2_7JQ',
+  authDomain: "minmatchhack.firebaseapp.com",
+  projectId: "minmatchhack",
+  storageBucket: "minmatchhack.appspot.com",
+  messagingSenderId: "247292901364",
+  appId: "1:247292901364:web:3ecfb95b1d407e34136ba1"
+};
+export default config;
